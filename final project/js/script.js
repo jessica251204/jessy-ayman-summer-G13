ebfa6a82498e93138b36075ed8618f6306a8a580
Jessica Ayman <jessy.ayman.ja@gmail.com>
2025-08-14 13:12:18 +0300

    final project

--- final project/js/script.js
+++ final project/js/script.js
@@ -7,16 +7,16 @@
   });
 });
 
-  const container = document.getElementById("scroll-container");
-  let scrollSpeed = 1; // pixels per frame
-  function autoScroll() {
-    container.scrollLeft += scrollSpeed;
-    if (container.scrollLeft >= container.scrollWidth - container.clientWidth) {
-      container.scrollLeft = 0; // loop back to start
-    }
-    requestAnimationFrame(autoScroll);
-  }
-  autoScroll();
+//   const container = document.getElementById("scroll-container");
+//   let scrollSpeed = 1; // pixels per frame
+//   function autoScroll() {
+//     container.scrollLeft += scrollSpeed;
+//     if (container.scrollLeft >= container.scrollWidth - container.clientWidth) {
+//       container.scrollLeft = 0; // loop back to start
+//     }
+//     requestAnimationFrame(autoScroll);
+//   }
+//   autoScroll();
 
    document.querySelectorAll(".thumb").forEach(img => {
     img.addEventListener("click", function() {
@@ -34,13 +34,28 @@
     }
   };
 
-  // Scroll smoothly to top
-  document.getElementById("backToTopBtn").addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
+//   Scroll smoothly to top
+//   document.getElementById("backToTopBtn").addEventListener("click", function () {
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+//   });
 
     document.querySelectorAll(".thumb").forEach(img => {
     img.addEventListener("click", function() {
       document.getElementById("mainImage").src = this.src.replace("80x100", "500x600");
+    });
+  });
+
+
+  // Category filter
+  document.querySelectorAll('.category-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+      const category = btn.getAttribute('data-category');
+      document.querySelectorAll('.product-item').forEach(item => {
+        if (category === 'all' || item.classList.contains(category)) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
     });
   });
